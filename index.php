41913575f1033da78abcee5a0dd9309434108b77
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2023-05-29 08:05:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,83 +1,94 @@
 <!DOCTYPE html>
-<html lang="ru">
-
 <head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Back 3</title>
+    <meta charset="utf-8" />
+    <title>Web3</title>
+    <link rel="stylesheet" href="style.css" type="text/css"/>
 </head>
 
-<body style="font-size: 20px;">
-	<form action="form.php" method="post">
-		<p>
-			<label>Пожалуйста, введите Ваше имя:<br><br>
-				<input placeholder="Имя" type="text" name="name" value="">
-			</label>
-		<p>
-			<label>Теперь Вашу электронную почту:<br><br>
-				<input placeholder="E-mail" type="text" name="email" value="">
-			</label>
-		</p>
-		<p>
-			<label>Укажите свой год рождения:<br><br>
-				<select name="year">
-					<option value="">Select...</option>
-					<?php
-					for ($i = 2008; $i >= 1900; --$i) {
-						echo "<option value='$i'>$i</option>";
-					}
-					?>
-				</select>
-			</label>
-		</p>
-		<p>Отметьте свой пол:<br><br>
-			<label>
-				<input type="radio" name="gender" value="man">Мужской
-			</label>
-			<label>
-				<input type="radio" name="gender" value="woman">Женский
-			</label>
-		</p>
-		<p>Укажите количество конечностей:<br><br>
-			<label>
-				<input type="radio" name="numlimbs" value="1">1
-			</label>
-			<label>
-				<input type="radio" name="numlimbs" value="2">2
-			</label>
-			<label>
-				<input type="radio" name="numlimbs" value="3">3
-			</label>
-			<label>
-				<input type="radio" name="numlimbs" value="4">4
-			</label>
-		</p>
-		<p>
-			<label>Какую сверхспособность Вы хотите?<br><br>
-				<select multiple name="super-powers[]">
-					<option value="immortality">Бессмертие</option>
-					<option value="walkthrough-walls">Прохождение сквозь стены</option>
-					<option value="levitation">Левитация</option>
-				</select>
-			</label>
-		</p>
-		<div>
-			<p>
-				<label>Расскажите немного о себе:<br><br>
-					<textarea placeholder="Расскажите о себе" name="biography"></textarea>
-				</label>
-			</p>
-		</div>
-		<p>
-			<label>
-				<input type="checkbox" name="agree">С контранктом и пользовательским соглашением ознакомлен
-				(а)
-			</label>
-		</p>
-		<p>
-			<input type="submit" value="Отправить и забыть">
-		</p>
-	</form>
-</body>
+<body>
+    <div class="main">
+    <h1>Форма</h1>
+    
+    <form action="index.php" method="POST">
+            <p>
+                Имя:
+                <input name="name" placeholder="Введите имя" />
+            </p>
 
-</html>
+            <p>
+                E-mail:
+                <input name="email" type="email" placeholder="Введите почту">
+            </p>
+
+            <p>
+                Год рождения:
+                <select id="yearB" name="year"></select>
+            </p>
+
+            <p>
+                Пол:
+                <input type="radio" name="radio-1" checked="checked" value="male" />
+                Мужской
+                <input type="radio" name="radio-1" value="female" />
+                Женский
+            </p>
+
+            <p>
+            Сколько конечностей?
+                
+                    <input type="radio" name="radio-2" checked="checked" value="4" />
+                    4
+
+                    <input type="radio" name="radio-2" value="3" />
+                    3
+
+                    <input type="radio" name="radio-2" value="2" />
+                    2
+
+                    <input type="radio" name="radio-2" value="1" />
+                    1
+
+                    <input type="radio" name="radio-2" value="0" />
+                    0
+                
+            </p>
+
+            <p>
+                Сверхспособности?
+                
+                    <select name="super" multiple="multiple">
+                    <option value="inv" selected="selected">Бессмертие</option>
+                    <option value="walk">прохождение сквозь стены</option>
+                    <option value="fly">левитация</option>
+                    </select>
+                
+            </p>
+
+            <p>
+                Биография?
+                <textarea name="bio"></textarea>
+            </p>
+
+            <p>
+                <input type="checkbox" name="check1" /> С контактом ознакомлен(а)
+            </p>
+
+            <p>
+            Отправка формы:
+            <input type="submit" value="Отправить" />
+            </p>
+        </form>
+    </div>
+    <script type="text/javascript">
+        window.onload = function () {
+        var yearB = document.getElementById("yearB");
+        var currentYear = (new Date()).getFullYear();
+        for (var i = 1950; i <= currentYear; i++) {
+            var option = document.createElement("OPTION");
+            option.innerHTML = i;
+            option.value = i;
+            yearB.appendChild(option);
+        }
+            };
+    </script>
+</body>
